da33dd56d447010cf8d1b8f39462fb8f1d06e5ad
HenriqueNas <henriquenas.dev@gmail.com>
2024-05-08 22:41:53 -0300

    test: :test_tube: add test for `/app` module

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:very_good_coffee_app/app/app.dart';
-import 'package:very_good_coffee_app/counter/counter.dart';
+import 'package:very_good_coffee_app/home/view/home_page.dart';
+
+import '../../helpers/helpers.dart';
 
 void main() {
-  group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+  setUpAll(DependenciesInjector.initDependencies);
+
+  group('VeryGoodCoffeeApp', () {
+    testWidgets('renders VeryGoodCoffeeApp', (tester) async {
+      await tester.pumpApp(const VeryGoodCoffeeApp());
+      expect(find.byType(HomePage), findsOneWidget);
     });
   });
 }
